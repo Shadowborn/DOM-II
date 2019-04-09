e0d4932c47971b58a54dc90195df8cde043e009c
Shadowborn <james.starks023@gmail.com>
2019-04-09 17:19:01 -0600

    wheel added

--- js/index.js
+++ js/index.js
@@ -2,67 +2,84 @@
 // nav anchors change color mouseover
 const navi = document.querySelector('nav');
 navi.addEventListener('mouseover', function (event) {
-      event.target.style.color = "blue";
-      setTimeout(function () {
-            event.target.style.color = "";
-      }, 400);
+    event.target.style.color = "blue";
+    setTimeout(function () {
+        event.target.style.color = "";
+    }, 400);
 }, false);
+
+// key down
+const headerP = document.querySelector('.intro p');
+document.addEventListener('keydown', function (event) {
+    if (event.code == 'KeyZ') {
+        headerP.style.color = "red";
+    }
+});
+
+// key up
+document.addEventListener('keyup', function (event) {
+    if (event.code == 'KeyZ') {
+        headerP.style.color = "";
+    }
+});
+
+//wheel
+const headerP2 = document.querySelector('.intro p');
+headerP2.addEventListener("wheel", wheel);
+    function wheel() {
+        this.style.fontSize = "35px";
+    }
+
+
+//drag / drop
+
+
 
 //nav anchors change font color on doubleclick
 navi.addEventListener('dblclick', function (event) {
-      event.target.style.color = "orange";
+    event.target.style.color = "orange";
 });
+
 
 //nav prevent default
 navi.addEventListener('click', function (event) {
-      event.preventDefault()
+    event.preventDefault()
 });
 
 //change img on hover with mouseovere/mouseout
 const busImg = document.querySelector('img');
 busImg.addEventListener('mouseover', () => {
-      event.target.setAttribute('src', 'img/funbus.png');
-      busImg.addEventListener('mouseout', () => {
-            event.target.setAttribute('src', 'img/fun-bus.jpg');
-      });
+    event.target.setAttribute('src', 'img/funbus.png');
+    busImg.addEventListener('mouseout', () => {
+        event.target.setAttribute('src', 'img/fun-bus.jpg');
+    });
 });
 
 //click bus pic vanish bus pic
 busImg.addEventListener('click', function (event) {
-      event.target.style.display = "none";
-      setTimeout(() => {
-            event.target.style.display = "";
-      }, 900);
+    event.target.style.display = "none";
+    setTimeout(() => {
+        event.target.style.display = "";
+    }, 900);
 }, false);
 
 //clicking logo refreshes page
 const logoRefresh = document.querySelector('.logo-heading');
 logoRefresh.addEventListener('click', () => {
-      window.setTimeout(() => {
-            window.location.reload(true);
-      }, 200);
+    window.setTimeout(() => {
+        window.location.reload(true);
+    }, 200);
 });
 
 //click footer vanish footer
 const foot = document.querySelector('footer');
 foot.addEventListener('click', function (event) {
-      event.target.style.display = "none";
-      setTimeout(() => {
-            event.target.style.display = "";
-      }, 900);
+    event.target.style.display = "none";
+    setTimeout(() => {
+        event.target.style.display = "";
+    }, 900);
 }, false);
 
-// key down
-const headerP = document.querySelector('p');
-document.addEventListener('keydown', function(event) {
-    if (event.code == 'KeyZ') {
-        headerP.style.color = "red";
-    }
-  });
 
-// key up
-document.addEventListener('keyup', function(event) {
-    if (event.code == 'KeyZ') {
-        headerP.style.color = "";
-    }
-  });
+
+//
